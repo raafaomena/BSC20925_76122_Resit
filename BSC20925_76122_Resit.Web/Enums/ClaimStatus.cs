7cca616238571413ed1bcf39ff5646a35fdf14ec
Rafaela Omena <rafaomena.s@gmail.com>
2026-07-08 13:44:23 +0100

    feat: add ClaimType and ClaimStatus enums
    - Added ClaimType enum with categories (Motor, Home, Travel, etc.)
    - Added ClaimStatus enum with workflow states (Submitted, InReview, etc.)
    - Both enums include integer values for database storage
    - Namespace: BSC20925_76122_Resit.Web.Models.Enums

---
+++ BSC20925_76122_Resit.Web/Enums/ClaimStatus.cs
@@ -0,0 +1,14 @@
+namespace BSC20925_76122_Resit.Web.Models.Enums;
+
+public enum ClaimStatus
+{
+    Submitted = 1,
+    InReview = 2,
+    UnderInvestigation = 3,
+    Approved = 4,
+    Rejected = 5,
+    Closed = 6,
+    PaymentPending = 7,
+    Paid = 8,
+    Cancelled = 9
+}
---
+++ BSC20925_76122_Resit.Web/Enums/ClaimType.cs
@@ -0,0 +1,12 @@
+namespace BSC20925_76122_Resit.Web.Models.Enums;
+
+public enum ClaimType
+{
+    Motor = 1,
+    Home = 2,
+    Travel = 3,
+    Health = 4,
+    Life = 5,
+    Property = 6,
+    Other = 7
+}
